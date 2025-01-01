1ba484b46d8e976b0369d1390ffcb6bba2166614
Devpoisons <devpoison@outlook.com>
2025-01-01 21:17:31 +0100

    -added textures -added more blocks o make it 54 -removed physics debugger -adjusted the camera position - added a reset buttom

--- src/script.js
+++ src/script.js
@@ -7,8 +7,6 @@ import CannonDebugger from 'cannon-es-debugger';
 /**
  * Base
  */
-// Debug
-const gui = new GUI()
 
 // Canvas
 const canvas = document.querySelector('canvas.webgl')
@@ -17,41 +15,59 @@ const canvas = document.querySelector('canvas.webgl')
 const scene = new THREE.Scene()
 
 /**
- * Objects/Physics for Jenga
+ * TEXTURES
+ */
+const textureLoader = new THREE.TextureLoader();
+
+//texture for bricks (oak_veneer_01_1k)
+const arm = textureLoader.load('./textures/oak_veneer_01_1k/oak_veneer_01_arm_1k.webp')
+arm.colorSpace = THREE.SRGBColorSpace
+
+const norm = textureLoader.load('./textures/oak_veneer_01_1k/oak_veneer_01_nor_gl_1k.webp')
+norm.colorSpace = THREE.SRGBColorSpace
+
+const map = textureLoader.load('./textures/oak_veneer_01_1k/oak_veneer_01_diff_1k.webp')
+map.colorSpace = THREE.SRGBColorSpace
+
+//textures for the floor (raw_plank_wall_1k)
+
+const floorArm = textureLoader.load('/textures/raw_plank_wall_1k/raw_plank_wall_arm_1k.webp')
+
+const floorMap = textureLoader.load('/textures/raw_plank_wall_1k/raw_plank_wall_diff_1k.webp')
+
+const floorNor = textureLoader.load('/textures/raw_plank_wall_1k/raw_plank_wall_nor_gl_1k.webp')
+
+floorNor.colorSpace = THREE.SRGBColorSpace
+floorArm.colorSpace = THREE.SRGBColorSpace
+floorMap.colorSpace = THREE.SRGBColorSpace
+
+/**
+ * Physics World for Jenga
  */
 
 const world = new CANNON.World({
     gravity: new CANNON.Vec3(0, -9.8, 0),
 })
 
-world.gravity.set(0, -5, 0)
+/**
+ * PHYSICS DEBUGGER
+ */
+// const cannonDebugger = new CannonDebugger(scene, world, {
+//     color: 0xff00f9
+// })
 
-// Max solver iterations: Use more for better force propagation, but keep in mind that it's not very computationally cheap!
-world.solver.iterations = 50
+//variables used by the function below
+const blocksPerRow = 3
 
-// Tweak contact properties.
-// Contact stiffness - use to make softer/harder contacts
-world.defaultContactMaterial.contactEquationStiffness = 5e6
+const blockWidth = 5
 
-// Stabilization time in number of timesteps
-world.defaultContactMaterial.contactEquationRelaxation = 3
-
-
-const cannonDebugger = new CannonDebugger(scene, world, {
-    color: 0xff00f9
-})
-
-const blocksPerRow = 3 // same as column
-
-const blockWidth = 3
-
-const blockHeight = 2
+const blockHeight = 3
 
 const blockBreadth = blockWidth * blocksPerRow
 
 const totalWidth = blocksPerRow * blockWidth
 
-const numOfRows = blocksPerRow * 3
+const numOfRows = 18
 
 const totalHeight = blockHeight * numOfRows
 
@@ -65,98 +81,121 @@ let block
 
 let body
 
-for (let row = 0; row < numOfRows; row++) {
-    for (let col = 0; col < blocksPerRow; col++) {
+//creates the Jendo
+const Jendo = () => {
+    for (let row = 0; row < numOfRows; row++) {
+        for (let col = 0; col < blocksPerRow; col++) {
 
-        // Handle Rotation for Even Rows
-        if (row % 2 == 0) {
+            // Handle Rotation for Even Rows
+            if (row % 2 == 0) {
 
-            // Position Calculations
+                // Position Calculations
 
-            //Determine the orientation of the row
-            let dx = 0; //places the block at horizontal from the camera
-            let dz = 1;
-            const gap = 0.07
-            const x = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dx
-            const y = (blockHeight + gap) * (row + 1);
-            const z = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dz;
+                //Determine the orientation of the row
+                let dx = 0; //places the block at horizontal from the camera
+                let dz = 1;
+                const gap = 0.07
+                const x = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dx
+                const y = (blockHeight + gap) * (row + 1);
+                const z = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dz;
 
-            const shape = new CANNON.Box(new CANNON.Vec3(blockBreadth / 2, blockHeight / 2, blockWidth / 2));
-            body = new CANNON.Body({
-                mass: 1, // Set a small mass for Jenga blocks
-                shape: shape,
-            });
-            body.velocity.set(0, 0, 0)
-
-
-            body.position.set(
-                x, // Center-align blocks along X or Z
-                y,                         // Stack rows along Y
-                z // Center-align blocks along Z or X
-            );
-
-            const blockGeo = new THREE.BoxGeometry(blockBreadth, blockHeight, blockWidth);
-            const blockMat = new THREE.MeshStandardMaterial({
-                color: 0x00ff00,
-                roughness: 0.3,
-                metalness: 0.1,
-            });
-            block = new THREE.Mesh(blockGeo, blockMat);
-
-            block.userData.physicsBody = body;
-
-        } else {
-            // Cannon-ES Physics Body
-
-            // Position Calculations
-
-            //Determine the orientation of the row
-            const dx = 1;//places the block at vertical from the camera
-            const dz = 0;
-            const gap = 0.07
-            const x = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dx
-            const y = (blockHeight + gap) * (row + 1);
-            const z = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dz;
-
-            const shape = new CANNON.Box(new CANNON.Vec3(blockWidth / 2, blockHeight / 2, blockBreadth / 2));
-            body = new CANNON.Body({
-                mass: 1, // Set a small mass for Jenga blocks
-                shape: shape,
-            });
-            body.velocity.set(0, 0, 0)
+                const shape = new CANNON.Box(new CANNON.Vec3(blockBreadth / 2, blockHeight / 2, blockWidth / 2));
+                body = new CANNON.Body({
+                    mass: 1, // Set a small mass for Jenga blocks
+                    shape: shape,
+                });
+                body.velocity.set(0, 0, 0)
 
 
-            body.position.set(
-                x, // Center-align blocks along X or Z
-                y,                         // Stack rows along Y
-                z // Center-align blocks along Z or X
-            );
+                body.position.set(
+                    x, // Center-align blocks along X or Z
+                    y,                         // Stack rows along Y
+                    z // Center-align blocks along Z or X
+                );
 
-            const blockGeo = new THREE.BoxGeometry(blockWidth, blockHeight, blockBreadth);
-            // Three.js Mesh
-            const blockMat = new THREE.MeshStandardMaterial({
-                color: 0x00ff00,
-                roughness: 0.3,
-                metalness: 0.1,
-            });
-            block = new THREE.Mesh(blockGeo, blockMat);
-            block.userData.physicsBody = body;
+                //create block meshes
+                const blockGeo = new THREE.BoxGeometry(blockBreadth, blockHeight, blockWidth);
+                const blockMat = new THREE.MeshStandardMaterial({
+                    map,
+                    roughnessMap: arm,
+                    aoMap: arm,
+                    metalnessMap: arm,
+                    normalMap: norm,
+                });
+                block = new THREE.Mesh(blockGeo, blockMat);
+
+                block.userData.physicsBody = body;
+
+            } else {
+                // Cannon-ES Physics Body
+
+                // Position Calculations
+
+                //Determine the orientation of the row
+                const dx = 1;//places the block at vertical from the camera
+                const dz = 0;
+                const gap = 0.07
+                const x = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dx
+                const y = (blockHeight + gap) * (row + 1);
+                const z = (blockWidth + gap) * (col - (blocksPerRow - 1) / 2) * dz;
+
+                const shape = new CANNON.Box(new CANNON.Vec3(blockWidth / 2, blockHeight / 2, blockBreadth / 2));
+                body = new CANNON.Body({
+                    mass: 1, // Set a small mass for Jenga blocks
+                    shape: shape,
+                });
+                body.velocity.set(0, 0, 0)
+
+
+                body.position.set(
+                    x, // Center-align blocks along X or Z
+                    y,                         // Stack rows along Y
+                    z // Center-align blocks along Z or X
+                );
+
+                const blockGeo = new THREE.BoxGeometry(blockWidth, blockHeight, blockBreadth);
+                // Three.js Mesh
+                const blockMat = new THREE.MeshStandardMaterial({
+                    map,
+                    roughnessMap: arm,
+                    aoMap: arm,
+                    metalnessMap: arm,
+                    normalMap: norm,
+                });
+                block = new THREE.Mesh(blockGeo, blockMat);
+                block.userData.physicsBody = body;
+            }
+
+
+
+            // Sync Three.js Mesh with Cannon Body
+            block.position.copy(body.position);
+            block.quaternion.copy(body.quaternion);
+
+            // Add to Scene and Physics World
+            scene.add(block);
+            world.addBody(body);
+
+            // Synchronize during the animation loop
+            blocks.push({ mesh: block, body: body });
         }
-
-
-
-        // Sync Three.js Mesh with Cannon Body
-        block.position.copy(body.position);
-        block.quaternion.copy(body.quaternion);
-
-        // Add to Scene and Physics World
-        scene.add(block);
-        world.addBody(body);
-
-        // Synchronize during the animation loop
-        blocks.push({ mesh: block, body: body });
     }
 }
+
+Jendo()
+
+
+//GUI Reset Button
+const gui = new GUI()
+
+const reset = {
+    reset: () => {
+        window.location.reload()
+    }
+}
+
+gui.add(reset, 'reset').name('reset')
+
 
 
 // Create a static ground plane
@@ -167,9 +206,24 @@ const groundBody = new CANNON.Body({
     type: CANNON.BODY_TYPES.STATIC
 });
 
-
 groundBody.quaternion.setFromEuler(-Math.PI / 2, 0, 0); // Rotate the plane to be horizontal
 world.addBody(groundBody);
+
+// Add a Three.js representation for the ground
+const groundGeo = new THREE.PlaneGeometry(200, 200);
+const groundMat = new THREE.MeshStandardMaterial({
+    side: THREE.DoubleSide,
+    map: floorMap,
+    aoMap: floorArm,
+    roughnessMap: floorArm,
+    metalnessMap: floorArm,
+    normalMap: floorNor,
+});
+const groundMesh = new THREE.Mesh(groundGeo, groundMat);
+groundMesh.rotation.x = -Math.PI / 2; // Match the rotation of the Cannon plane
+scene.add(groundMesh);
+scene.position.y = -10 //moved the scene downwards
+
 
 /**
  * Sizes
@@ -179,14 +233,6 @@ const sizes = {
     height: window.innerHeight
 }
 
-// Add a Three.js representation for the ground
-const groundGeo = new THREE.PlaneGeometry(50, 50);
-const groundMat = new THREE.MeshStandardMaterial({ color: 0x808080, side: THREE.DoubleSide });
-const groundMesh = new THREE.Mesh(groundGeo, groundMat);
-groundMesh.rotation.x = -Math.PI / 2; // Match the rotation of the Cannon plane
-scene.add(groundMesh);
-
-
 /**
  * LIGHT
  */
@@ -194,6 +240,7 @@ const ambientLight = new THREE.AmbientLight(0xffffff, 1)
 const directionalLight = new THREE.DirectionalLight(0xffffff, 1)
 scene.add(ambientLight, directionalLight)
 directionalLight.position.set(-2, 0.3, 0)
+
 
 /**
  * RAYCASTER
@@ -203,8 +250,6 @@ const raycaster = new THREE.Raycaster()
 
 //init a pointer 2d
 const pointer = new THREE.Vector2()
-
-const forceMagnitude = 108
 
 window.addEventListener('click', (ev) => {
     pointer.x = (ev.clientX / sizes.width) * 2 - 1
@@ -221,7 +266,6 @@ window.addEventListener('click', (ev) => {
         console.log(physicsBody.position)
 
         if (physicsBody) {
-
             intersectedObject.material.color.set(0xff0000)
             setTimeout(() => {
                 // Remove the body from the physics world
@@ -231,13 +275,16 @@ window.addEventListener('click', (ev) => {
                 if (intersectedObject && intersectedObject.parent) {
                     intersectedObject.parent.remove(intersectedObject);
                 }
-            }, 500)
+            }, 150)
 
         }
     }
 })
 
 
+/**
+ * RESIZE
+ */
 window.addEventListener('resize', () => {
     // Update sizes
     sizes.width = window.innerWidth
@@ -252,19 +299,22 @@ window.addEventListener('resize', () => {
     renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
 })
 
+
 /**
  * Camera
  */
 // Base camera
-const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 100)
-camera.position.z = 36
-camera.position.y = -2
+const camera = new THREE.PerspectiveCamera(75, sizes.width / sizes.height, 0.1, 1000)
+camera.position.z = 60
+camera.position.y = 70
 
 scene.add(camera)
+
 
 // Controls
 const controls = new OrbitControls(camera, canvas)
 controls.enableDamping = true
+
 
 /**
  * Renderer
@@ -275,17 +325,14 @@ const renderer = new THREE.WebGLRenderer({
 })
 renderer.setSize(sizes.width, sizes.height)
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+renderer.setClearColor(new THREE.Color(0xd808080), 1)
 
 /**
  * Animate
  */
-const clock = new THREE.Clock()
 
 const tick = () => {
-    const elapsedTime = clock.getElapsedTime()
-
     world.step(1 / 60, 0.01, 10); // Increase the number of iterations (3rd parameter)
-
 
     // Sync Three.js meshes with Cannon bodies
     blocks.forEach(({ mesh, body }) => {
@@ -296,7 +343,7 @@ const tick = () => {
     // Update controls
     controls.update()
 
-    cannonDebugger.update()
+    // cannonDebugger.update()
 
     // Render
     renderer.render(scene, camera)
